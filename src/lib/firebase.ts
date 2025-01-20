2e09d3e175caed9d773986325ef4fb9b6d72a940
lucad-28 <lucadev.rll28@gmail.com>
2025-01-19 20:09:18 -0500

    Fix credentials

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,6 +2,15 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID,
+};
 
 export const app =
   getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
